58f8a88f976bb41fe21cdec4ee3acb7219ba5d4f
Madison Bentley <mb.madisonbentley@gmail.com>
2020-05-29 21:54:32 -0700

    preliminrary demarkation of divs

---
+++ src/components/Cli.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Cli = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(event.target.query.value)
+  }
+  return(
+    <div className='cli'>
+      <form onSubmit={handleSubmit}>
+        <input name='query' />
+        <button type='submit'>Search</button>
+      </form>
+    </div>
+  )
+}
+export default Cli
