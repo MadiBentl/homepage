2c1335b8af8543d02fd0c6f8471ac3c6768a5d68
Madison Bentley <mb.madisonbentley@gmail.com>
2020-06-01 18:08:29 -0700

    add item completion and styling

--- src/components/features/Notepad.js
+++ src/components/features/Notepad.js
@@ -1,22 +1,25 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { toggleNoteStatus } from '../../reducers/notepad'
 
-const Note = ({ note }) => {
+const Note = ({ note, handleClick }) => {
   return(
-    <li>
+    <li onClick = {handleClick} className={note.complete ? 'text-strike' : null}
+>
       {note.content}
     </li>
   )
 }
 
 const Notepad = () => {
+  const dispatch = useDispatch()
   const notepadData = useSelector(state => state.notepad)
   return(
     <div>
       <h1>{ notepadData.name }</h1>
       <ul>
         {notepadData.notes.map(note =>
-          <Note key={note.content} note={note} />
+          <Note key={note.id} note={note} handleClick={() => dispatch(toggleNoteStatus(note.id)) }/>
         )}
       </ul>
     </div>
--- src/reducers/notepad.js
+++ src/reducers/notepad.js
@@ -1,24 +1,41 @@
+const generateId = () => {
+  return Math.round(Math.random() * 9999999999)
+}
+
 const notepadReducer = (state = {}, action) => {
   switch(action.type){
-    case 'CREATE_NOTEPAD':
-      return {
-        name: 'Notepad',
-        visible: true,
-        notes: []
-      }
-    case 'DELETE_NOTEPAD':
-      return {}
-    case 'HIDE_NOTEPAD':
-      return { ...state, visible: false }
-    case 'SHOW_NOTEPAD':
-      return { ...state, visible: true }
-    case 'ADD_NOTE':
-      return { ...state, notes: state.notes.concat({
-        content: action.content,
-        status: 'incomplete'
-      }) }
-    default:
-      return state
+  case 'CREATE_NOTEPAD':
+    return {
+      name: 'Notepad',
+      visible: true,
+      notes: []
+    }
+  case 'DELETE_NOTEPAD':
+    return {}
+  case 'HIDE_NOTEPAD':
+    return { ...state, visible: false }
+  case 'SHOW_NOTEPAD':
+    return { ...state, visible: true }
+  case 'ADD_NOTE':
+    return { ...state, notes: state.notes.concat({
+      content: action.data.content,
+      complete: false,
+      id: generateId()
+    }) }
+  case 'TOGGLE_STATUS':
+    const noteToToggle = state.notes.find(n => n.id === action.data.id)
+    const toggledNote = {
+      ...noteToToggle,
+      complete: !noteToToggle.complete
+    }
+    console.log('toggled', toggledNote)
+    const newNotes = state.notes.map(n =>
+      n.id === action.data.id ? toggledNote : n
+    )
+    console.log(newNotes)
+    return { ...state, notes: newNotes }
+  default:
+    return state
   }
 }
 
@@ -35,7 +52,10 @@ export const showNotepad = () => {
   return({ type: 'SHOW_NOTEPAD' })
 }
 export const addNote = (content) => {
-  return({ type: 'ADD_NOTE', content })
+  return({ type: 'ADD_NOTE', data:{ content } })
+}
+export const toggleNoteStatus = (id) => {
+  return({ type: 'TOGGLE_STATUS', data:{ id } })
 }
 
 export default notepadReducer
