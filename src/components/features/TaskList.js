d1da013cbb57b2ff14a298380cb638982da5d711
Madison Bentley <mb.madisonbentley@gmail.com>
2020-06-18 13:40:07 -0700

    added input field settings to tasklist

--- src/components/features/TaskList.js
+++ src/components/features/TaskList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { toggleTask } from '../../reducers/tasklist'
 
@@ -12,14 +12,37 @@ const Task = ({ task }) => {
   )
 }
 
+const NewTask = ({ setNewTask, newTask }) => {
+  return (
+    <div className = 'ui transparent checkbox input'>
+      <input type='checkbox' />
+      <label>
+        <div className = 'ui transparent input'>
+          <input value={newTask.content} placeholder='add new task' onChange = {(event) => setNewTask({ active: true, content: event.target.value })}/>
+        </div>
+      </label>
+    </div>
+  )
+}
+
 const TaskList = () => {
   const taskData = useSelector(state => state.taskList)
+  const [newTask, setNewTask] = useState({ active: false, content: '' })
+  const handleClick = () => {
+    setNewTask({ ...newTask, active: true })
+  }
   return(
-    <div className='feature tasklist'>
+    <div
+      className="feature tasklist"
+      onClick = {() => handleClick()}
+    >
       <h3>TaskList for {taskData.name}</h3>
       {taskData.tasks.map(task => {
         return <Task key={task.id} task={task} />
       })}
+      {newTask.active ?
+        <NewTask setNewTask={setNewTask} newTask={newTask}/>
+        : null}
     </div>
   )
 }
