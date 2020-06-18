3a478e1a939006d0190ea2e90fc2c160bd241ec0
Madison Bentley <mb.madisonbentley@gmail.com>
2020-06-18 11:35:33 -0700

    fixed timer bug where it would cycle through different times before starting

--- src/components/features/Timer.js
+++ src/components/features/Timer.js
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { stopTimer, pauseTimer, resumeTimer } from '../../reducers/timer'
+import { pauseTimer, resumeTimer } from '../../reducers/timer'
 
 
 const Timer = () => {
   const timerData = useSelector(state => state.timer)
-  const [timeToDisplay, setTimeToDisplay] = useState(timerData.initialTime)
-  const [distance, setDistance] = useState(0)
+  const [distance, setDistance] = useState(timerData.initialTime)
   const dispatch = useDispatch()
 
   let interval = null
   const now = timerData.timeWhenCreated
+
+  const formatTime = (milliseconds) => {
+    let days = Math.floor(milliseconds / (1000 * 60 * 60 * 24)) ? Math.floor(milliseconds / (1000 * 60 * 60 * 24)) + ':' : ''
+    var hours = ('0' + Math.floor((milliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).slice(-2)
+    var minutes = ('0' + Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60))).slice(-2)
+    var seconds = ('0' + Math.floor((milliseconds % (1000 * 60)) / 1000)).slice(-2)
+
+    return days + hours + ':' + minutes + ':' + seconds
+  }
 
   useEffect(() => {
     if (timerData.isOn){
@@ -19,15 +27,7 @@ const Timer = () => {
         const finishedTime = (timerData.initialTime) + now
         setDistance(finishedTime - startingTime)
 
-        let days = Math.floor(distance / (1000 * 60 * 60 * 24)) ? Math.floor(distance / (1000 * 60 * 60 * 24)) + ':' : ''
-        var hours = ('0' + Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).slice(-2)
-        var minutes = ('0' + Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))).slice(-2)
-        var seconds = ('0' + Math.floor((distance % (1000 * 60)) / 1000)).slice(-2)
-
-        const timeLeft = days + hours + ':' + minutes + ':' + seconds
-        if (distance >= 0){
-          setTimeToDisplay(timeLeft)
-        } else {
+        if (distance <= 0){
           clearInterval(interval)
         }
       }, 1000)
@@ -36,30 +36,21 @@ const Timer = () => {
       clearInterval(interval)
     }
     return () => clearInterval(interval)
-  }, [timerData.isOn, timeToDisplay])
+  }, [timerData.isOn, distance])
 
-  const handleStop = () => {
-    dispatch(stopTimer())
-    setTimeToDisplay('0:00:00')
-  }
   const handlePause = () => {
     dispatch(pauseTimer(distance))
     clearInterval(interval)
   }
   return(
     <div className = 'feature timer sidebar'>
-      <h2>{ timeToDisplay }</h2>
+      <h2>{ formatTime(distance) }</h2>
       <p>{ timerData.name }</p>
       <div>
         <i
           aria-hidden="true"
           className="pause icon large"
           onClick={() => handlePause() }
-        ></i>
-        <i
-          aria-hidden="true"
-          className="stop icon large"
-          onClick={() => handleStop()}
         ></i>
         <i
           aria-hidden="true"
