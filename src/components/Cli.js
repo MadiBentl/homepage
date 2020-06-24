04a376117a3fd8c16c32fb14ea18426fd0cabcda
Madison Bentley <mb.madisonbentley@gmail.com>
2020-06-24 13:06:33 -0700

    removed downshift component as it was buggy

--- src/components/Cli.js
+++ src/components/Cli.js
@@ -21,83 +21,90 @@ const items = [
 ]
 
 const Cli = () => {
+  const [query, setQuery] = useState('')
   const dispatch = useDispatch()
+
   const handleSubmit = (e) => {
     e.preventDefault()
+    switch(query){
+      case 'randomize background':
+        dispatch(setWallpaper())
+        break
+      case 'change background':
+        dispatch(setWallpaper())
+        break
+      case 'set timer':
+        dispatch(createTimer(20))
+        break
+      case 'hide timer':
+        dispatch(toggleTimer())
+        break
+      case 'show timer':
+        dispatch(toggleTimer())
+        break
+      case 'create tasklist':
+        dispatch(createTasklist())
+        break
+      case 'show weather':
+        dispatch(getWeather('94', '129'))
+        break
+      default:
+        break
+    }
   }
   return(
-    <Downshift
-      onChange={selection => {
-        if (selection) {
-          switch(selection.value){
-            case 'randomize background':
-              dispatch(setWallpaper())
-              break
-            case 'change background':
-              break
-            case 'set timer':
-              console.log('selection', selection)
-              dispatch(createTimer(20))
-              break
-            case 'hide timer':
-              dispatch(toggleTimer())
-              break
-            case 'show timer':
-              dispatch(toggleTimer())
-              break
-            case 'create tasklist':
-              dispatch(createTasklist())
-              break
-            case 'show weather':
-              dispatch(getWeather('94', '129'))
-              break
-            default:
-              break
-          }
-        }
-      }}
-      itemToString={item => (item ? item.value : '')}
-    >
-      {({
-        getInputProps,
-        getItemProps,
-        getMenuProps,
-        isOpen,
-        inputValue,
-        highlightedIndex,
-        selectedItem,
-      }) => (
-        <div className='cli ui transparent'>
-          <div className='ui relaxed divided list cli-options' {...getMenuProps()}>
-            {isOpen
-              ? items
-                .filter(item => !inputValue || item.value.includes(inputValue))
-                .map((item, index) => (
-                  <div className='item'
-                    key={ item.value }
-                    {...getItemProps({
-                      index,
-                      item,
-                      style: {
-                        backgroundColor:
-                        highlightedIndex === index ? 'rgba(255, 255, 255, 0.3)' : null,
-                        fontWeight: selectedItem === item ? 'bold' : 'normal',
-                      },
-                    })}
-                  >
-                    {item.value}
-                  </div>
-                ))
-              : null}
-          </div>
-          <form onSubmit = {handleSubmit}>
-            <input name='query' placeholder='example: create timer 10' {...getInputProps()} />
-            <button type='submit' className='ui inverted button'>Go</button>
-          </form>
-
-        </div>
-      )}
-    </Downshift>
+    <div className='cli ui transparent'>
+      <div className='ui relaxed divided list cli-options'>
+        {query.length
+          ? items
+            .filter(item => !query || item.value.includes(query))
+            .map((item, index) => (
+              <div className='item'
+                key={ item.value }
+                index={ index }
+              >
+                {item.value}
+              </div>
+            ))
+          : null}
+      </div>
+      <form onSubmit = {handleSubmit}>
+        <input
+          name='query'
+          value={query}
+          placeholder='example: create timer 10'
+          onChange={e => setQuery(e.target.value)}/>
+        <button type='submit' className='ui inverted button'>Go</button>
+      </form>
+    </div>
   )
 }
 export default Cli
+
+/*
+switch(selection.value){
+  case 'randomize background':
+    dispatch(setWallpaper())
+    break
+  case 'change background':
+    break
+  case 'set timer':
+    console.log('selection', selection)
+    dispatch(createTimer(20))
+    break
+  case 'hide timer':
+    dispatch(toggleTimer())
+    break
+  case 'show timer':
+    dispatch(toggleTimer())
+    break
+  case 'create tasklist':
+    dispatch(createTasklist())
+    break
+  case 'show weather':
+    dispatch(getWeather('94', '129'))
+    break
+  default:
+    break
+}
+*/
