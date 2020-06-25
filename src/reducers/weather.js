69570f527c448c4106ce63e00d08f087089701f1
Madison Bentley <mb.madisonbentley@gmail.com>
2020-06-25 13:23:21 -0700

    added weather functionality that works with api call

--- src/reducers/weather.js
+++ src/reducers/weather.js
@@ -6,7 +6,7 @@ const weatherReducer = (state={}, action) => {
       return {
         location: action.data.name,
         temperature: action.data.main.temp,
-        weather: action.data.weather[0].main,
+        weather: action.data.weather[0].description,
         day: new Date(),
         visible: true
       }
@@ -22,15 +22,20 @@ const weatherReducer = (state={}, action) => {
   }
 }
 
-export const getWeather = (lat, lon) => {
+export const getWeather = () => {
   return async dispatch => {
     const API_KEY = '7341f66d131be4663e75d126e95b4ed0'
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=49&lon=-129&appid=${API_KEY}`)
-    console.log(response)
-    dispatch({
-      type: 'INIT_WEATHER',
-      data: response.data
-    })
+    if (navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(async position => {
+        console.log(position)
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${API_KEY}`)
+        console.log(response)
+        dispatch({
+          type: 'INIT_WEATHER',
+          data: response.data
+        })
+      })
+    }
   }
 }
 
