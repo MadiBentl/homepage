f78646fdadad46d85d8725313bc6e6059eff750b
Madison Bentley <mb.madisonbentley@gmail.com>
2020-07-02 18:24:31 -0700

    added randomization to bg

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,9 +1,16 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { setWallpaper } from '../reducers/canvas'
 import Footer from './Footer'
 
 const Canvas = (props) => {
   const image = useSelector(state => state.canvas)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(setWallpaper())
+  }, [])
+
   return (
     <div className='canvas lazy-bg' style={{ backgroundImage : `url(${image.img})` }}>
       {props.children}
