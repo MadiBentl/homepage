c35a237549d121e8fc6962c6210e7ac6820f3038
Madison Bentley <mb.madisonbentley@gmail.com>
2020-07-04 11:06:01 -0700

    got log in to work

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import logo from '../assets/logo-white.png'
 import { openModal } from '../reducers/admin'
+import Login from './Login'
 
 const Header = () => {
   const dispatch = useDispatch()
   return(
     <div className='header'>
       <img className='logo' src ={logo} alt='logo' />
-      <div className = 'login' onClick={() => dispatch(openModal())}>
-          Login
+      <div className = 'login'>
+        <Login />
       </div>
     </div>
   )
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Modal from './Modal'
 
 const Login = () => {
-  let auth = ''
+
+  const [auth, setAuth] = useState('')
+
   useEffect(() => {
     window.gapi.load('client:auth2',() => {
       window.gapi.client.init({
         'clientId': '94631634763-hnonm7ldimcfbnh9m5t7l5gff4vhsufi.apps.googleusercontent.com',
         'scope': 'email'
       }).then(() => {
-        auth = window.gapi.auth2.getAuthInstance()
-        console.log(auth)
+        setAuth(window.gapi.auth2.getAuthInstance())
+        console.log('useeffect', auth)
       })
     })
   }, [])
 
-  const loginInfo = useSelector(state => state.admin)
-  if (loginInfo.visible){
-    return(
-      <p>hi</p>
-    )
-  }else{
-    return null
+  const handleClick = () => {
+    console.log('auth', auth)
+    auth.signIn()
   }
+
+  return(
+    <div onClick= {() => handleClick()}>Login</div>
+  )
 }
 
 export default Login
