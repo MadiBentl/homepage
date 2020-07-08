829fa6206803b9235296ca2aca4fa0a3e2c194f4
Madison Bentley <mb.madisonbentley@gmail.com>
2020-07-08 10:45:02 -0700

    add get date function to admin

--- src/reducers/admin.js
+++ src/reducers/admin.js
@@ -4,6 +4,8 @@ const adminReducer = (state = { user: null }, action ) => {
       return { ...state, user: true, userId: action.data.userId }
     case 'LOGOUT':
       return { ...state, user: false }
+    case 'GET_DAY':
+      return { ...state, today: new Date().toDateString() }
     default:
       return state
   }
@@ -14,6 +16,9 @@ export const setLogIn = (userId) => {
 }
 export const setLogOut = () => {
   return ({ type: 'LOGOUT' })
+}
+export const isNewDay = () => {
+  return ({ type: 'GET_DAY' })
 }
 
 export default adminReducer
