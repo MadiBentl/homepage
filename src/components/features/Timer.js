4d6cb4f60b96a10c0de39f1e399e5ec864d7afa2
Madison Bentley <mb.madisonbentley@gmail.com>
2020-07-10 18:48:23 -0700

    change autocomplete to autoComplete

--- src/components/features/Timer.js
+++ src/components/features/Timer.js
@@ -79,7 +79,7 @@ const Timer = () => {
             onAccept={
               (value) => setUserTimerInput(value)
             }
-            autocomplete="off"
+            autoComplete="off"
             // ...and more mask props in a guide
 
             // input props also available
